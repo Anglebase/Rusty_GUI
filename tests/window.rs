142bb2be42bf0f540439326d3442531aa1e8be2d
Anglebase <1628818324@qq.com>
2024-11-30 14:23:54 +0800

    update test

--- tests/window.rs
+++ tests/window.rs
@@ -26,3 +26,53 @@ fn window_creation() {
     w.show();
     App::run();
 }
+
+#[test]
+fn ss_demo() {}
+
+use std::time;
+
+struct MyWin {}
+
+impl WinProc for MyWin {
+    fn init(&mut self) {
+        println!("init");
+    }
+    fn destroy(&mut self) {
+        println!("destroy");
+    }
+    fn draw(&mut self, g: &mut Graphics) {
+        g.full_clear(Color::WHITE);
+        let p = Pen::new(PenStyle::Solid, 3, Color::BLACK);
+        g.apply_pen(&p);
+        let pt = p! {
+            g.get_rect().size.width/2,
+            g.get_rect().size.height/2,
+        };
+        let theta = time::SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            % 1000000;
+        let theta = theta as f32 / 1000.0;
+        g.line(
+            pt,
+            pt + p! {
+                (theta.sin() * 200.0) as i32,
+                (theta.cos() * 200.0) as i32,
+            },
+        );
+        let fs: FontStyle = Default::default();
+        let font = Font::new(fs);
+        g.apply_font(&font);
+
+        g.text("你好, Rusty GUI!", p! {10,10});
+    }
+}
+
+#[test]
+fn update_test() {
+    let w = Window::new(Box::new(MyWin {}), None);
+    w.show();
+    App::run();
+}
