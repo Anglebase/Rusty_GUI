28f5707fbc09e11cec1e3d8bce0cf35e1b8a97f3
Anglebase <1628818324@qq.com>
2024-11-30 14:20:00 +0800

    Implement text related parts

--- src/graph.rs
+++ src/graph.rs
@@ -1,3 +1,4 @@
+use crate::core::string_to_wchar;
 use std::ptr::null_mut;
 
 /// This file contains system API interactions and encapsulation based on device handles.
@@ -28,14 +29,14 @@ macro_rules! rgb {
 }
 
 impl Color {
-    pub const BLACK: Color = rgb!{ 0, 0, 0 };
-    pub const WHITE: Color = rgb!{ 255, 255, 255 };
-    pub const RED: Color = rgb!{ 255, 0, 0 };
-    pub const GREEN: Color = rgb!{ 0, 255, 0 };
-    pub const BLUE: Color = rgb!{ 0, 0, 255 };
-    pub const YELLOW: Color = rgb!{ 255, 255, 0 };
-    pub const CYAN: Color = rgb!{ 0, 255, 255 };
-    pub const MAGENTA: Color = rgb!{ 255, 0, 255 };
+    pub const BLACK: Color = rgb! { 0, 0, 0 };
+    pub const WHITE: Color = rgb! { 255, 255, 255 };
+    pub const RED: Color = rgb! { 255, 0, 0 };
+    pub const GREEN: Color = rgb! { 0, 255, 0 };
+    pub const BLUE: Color = rgb! { 0, 0, 255 };
+    pub const YELLOW: Color = rgb! { 255, 255, 0 };
+    pub const CYAN: Color = rgb! { 0, 255, 255 };
+    pub const MAGENTA: Color = rgb! { 255, 0, 255 };
 }
 
 pub struct Pen {
@@ -92,16 +93,74 @@ impl Brush {
     }
 }
 
-// pub struct Font {
-//     pub(crate) hfont: HFONT,
-// }
-// impl Drop for Font {
-//     fn drop(&mut self) {
-//         unsafe {
-//             DeleteObject(self.hfont as HGDIOBJ);
-//         }
-//     }
-// }
+pub struct Font {
+    pub(crate) hfont: HFONT,
+}
+pub enum FontWeight {
+    Dontcare = FW_DONTCARE as isize,
+    Thin = FW_THIN as isize,
+    ExtraLight = FW_EXTRALIGHT as isize,
+    Light = FW_LIGHT as isize,
+    Normal = FW_NORMAL as isize,
+    Medium = FW_MEDIUM as isize,
+    SemiBold = FW_SEMIBOLD as isize,
+    Bold = FW_BOLD as isize,
+    ExtraBold = FW_EXTRABOLD as isize,
+    Black = FW_BLACK as isize,
+}
+pub struct FontStyle {
+    pub size: i32,
+    pub weight: FontWeight,
+    pub italic: bool,
+    pub underline: bool,
+    pub strikeout: bool,
+    pub font: String,
+}
+
+impl Drop for Font {
+    fn drop(&mut self) {
+        unsafe {
+            DeleteObject(self.hfont as HGDIOBJ);
+        }
+    }
+}
+
+impl Default for FontStyle {
+    fn default() -> Self {
+        Self {
+            size: 16,
+            weight: FontWeight::Normal,
+            italic: false,
+            underline: false,
+            strikeout: false,
+            font: String::from("宋体"),
+        }
+    }
+}
+
+impl Font {
+    pub fn new(style: FontStyle) -> Self {
+        let hfont = unsafe {
+            CreateFontW(
+                style.size,
+                0,
+                0,
+                0,
+                style.weight as i32,
+                style.italic as u32,
+                style.underline as u32,
+                style.strikeout as u32,
+                DEFAULT_CHARSET,
+                OUT_DEFAULT_PRECIS,
+                CLIP_DEFAULT_PRECIS,
+                DEFAULT_QUALITY,
+                DEFAULT_PITCH | FF_DONTCARE,
+                string_to_wchar(style.font.as_str()).as_ptr(),
+            )
+        };
+        Self { hfont }
+    }
+}
 
 impl Graphics {
     pub fn get_rect(&self) -> Rect {
@@ -119,7 +178,6 @@ impl Graphics {
             }
         }
     }
-
     pub fn apply_pen(&self, pen: &Pen) {
         unsafe {
             SelectObject(self.hdc, pen.hpen as HGDIOBJ);
@@ -128,6 +186,11 @@ impl Graphics {
     pub fn apply_brush(&self, brush: &Brush) {
         unsafe {
             SelectObject(self.hdc, brush.hbrush as HGDIOBJ);
+        }
+    }
+    pub fn apply_font(&self, font: &Font) {
+        unsafe {
+            SelectObject(self.hdc, font.hfont as HGDIOBJ);
         }
     }
 
@@ -149,6 +212,18 @@ impl Graphics {
         unsafe {
             MoveToEx(self.hdc, p1.x, p1.y, null_mut());
             LineTo(self.hdc, p2.x, p2.y);
+        }
+    }
+    pub fn text(&self, text: &str, p: Point) {
+        let s = string_to_wchar(text);
+        unsafe {
+            TextOutW(
+                self.hdc,
+                p.x,
+                p.y,
+                s.as_ptr(),
+                s.len() as i32 - 1,
+            );
         }
     }
 }
