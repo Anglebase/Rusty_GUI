25027f91d46ea86cf5d97871c127b93595aef8cc
Anglebase <1628818324@qq.com>
2024-12-08 23:08:25 +0800

    add event support

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ mod graphics;
 mod types;
 mod window;
 mod winimpl;
+mod events;
 
 pub use app::App;
 pub use graphics::*;
 pub use types::{Color, Point, Rect, Size};
 pub use window::Window;
 pub use winimpl::{WinImpl, WinProc};
+pub use events::*;
