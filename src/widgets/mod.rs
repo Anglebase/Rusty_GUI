9d401b9504c53ec0ac74b5b8a5dcce182c037f51
Anglebase <1628818324@qq.com>
2024-12-27 16:02:01 +0800

    add switch

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,5 +1,7 @@
 mod block;
 mod pushbutton;
+mod switch;
 
 pub use block::Block;
 pub use pushbutton::PushButton;
+pub use switch::Switch;
---
+++ src/widgets/switch.rs
@@ -0,0 +1,86 @@
+use crate::*;
+
+/// demo wigdet
+pub struct Switch {
+    this: Window,
+    label: String,
+    status: bool,
+    bkcolor: Color,
+    pub state_changed: Notifier<bool>,
+}
+
+impl Switch {
+    pub fn new(label: &str, rect: Rect, parent: &Window) -> Widget<Self> {
+        let mut this = Widget::new(Box::new(Self {
+            this: Window::default(),
+            label: label.to_string().clone(),
+            status: false,
+            state_changed: Notifier::new(),
+            bkcolor: rgb!(235),
+        }));
+        *this.as_window_mut() = Window::new(label, rect, Some(parent), &this);
+        this
+    }
+}
+
+default_userdata!(Switch);
+default_aswindow!(Switch, this);
+
+impl Drawable for Switch {
+    fn draw(&mut self, canvas: &mut crate::Canvas) {
+        let rect = self.as_window().rect();
+        canvas.clear(if self.status { Color::LIGHT_GRAY } else { self.bkcolor });
+        let fs = FontStyle {
+            size: 24,
+            ..Default::default()
+        };
+        let fo = Font::new(fs);
+        canvas.set_font(&fo);
+        let text_rect = if self.status {
+            Rect {
+                pos: rect.pos + Point { x: 1, y: 1 },
+                size: rect.size,
+            }
+        } else {
+            rect
+        };
+        canvas.rect_text(text_rect, &self.label, TextAlign::Center);
+        if !self.status {
+            let ls = Pen::new(PenStyle {
+                width: 2,
+                color: Color::DARK_GRAY,
+                ..Default::default()
+            });
+            canvas.set_pen(&ls);
+            canvas.line(rect.right(), rect.top(), rect.right(), rect.bottom());
+            canvas.line(rect.left(), rect.bottom(), rect.right(), rect.bottom());
+        }
+    }
+}
+
+impl EventListener for Switch {
+    fn on_event(&mut self, event: &crate::Event) {
+        match event {
+            Event::MouseButtonPressed {
+                button,
+                pos: _,
+                mk: _,
+            } => {
+                if *button == MouseButton::Left {
+                    self.status = !self.status;
+                    self.state_changed.notify(&self.status);
+                    self.this.update();
+                }
+            }
+            Event::Hover { pos: _, mk: _ } => {
+                self.bkcolor = rgb!(215);
+                self.this.update();
+            }
+            Event::Leave => {
+                self.bkcolor = rgb!(235);
+                self.this.update();
+            }
+            _ => {}
+        }
+    }
+}
