0aa8aa53b21dd37b3650ab29f47b7aac3e338e2c
Anglebase <1628818324@qq.com>
2025-01-13 11:37:20 +0800

    feat: Add Row Layout and Testing

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -2,8 +2,21 @@ mod block;
 mod pushbutton;
 mod switch;
 mod lineedit;
+mod row;
 
 pub use block::Block;
 pub use pushbutton::PushButton;
 pub use switch::Switch;
 pub use lineedit::LineEdit;
+pub use row::Row;
+
+#[derive(Debug, Clone, Copy)]
+pub enum LayoutMode {
+    Fixed(i32),
+    Ratio(f32),
+    Range{
+        min: Option<i32>,
+        max: Option<i32>,
+        ratio: f32,
+    },
+}
---
+++ src/widgets/row.rs
@@ -0,0 +1,210 @@
+use std::{collections::HashMap, i32};
+
+use crate::{widgets::LayoutMode, *};
+
+pub struct Row {
+    this: Window,
+    layouts: Vec<(WindowID, LayoutMode)>,
+    padding: i32,
+    spacing: i32,
+}
+
+impl Default for Row {
+    fn default() -> Self {
+        Self {
+            this: Window::default(),
+            layouts: vec![],
+            padding: 5,
+            spacing: 3,
+        }
+    }
+}
+
+default_as_window!(Row);
+
+impl Drawable for Row {
+    fn draw(&mut self, canvas: &mut Canvas) {
+        let _ = canvas;
+    }
+}
+
+impl EventListener for Row {
+    fn on_event(&mut self, event: &Event) {
+        if let Event::WindowResized { .. } = event {
+            self.update(self.this.rect().size);
+        }
+    }
+}
+
+impl Row {
+    fn update(&mut self, size: Size) {
+        let size = size!(size.width, size.height);
+        // If there is no layout, do nothing
+        if self.layouts.is_empty() {
+            return;
+        }
+        // Calculate the minimum and maximum width of the row
+        let mut min_width = 0;
+        let mut need_max = true;
+        let mut max_width = 0;
+        for &(_, mode) in &self.layouts {
+            match mode {
+                LayoutMode::Fixed(width) => {
+                    min_width += width;
+                }
+                LayoutMode::Ratio(_) => {
+                    need_max = false;
+                }
+                LayoutMode::Range { min, max, .. } => {
+                    min_width += min.unwrap_or(0);
+                    max_width += match max {
+                        Some(max) => max,
+                        None => {
+                            need_max = false;
+                            0
+                        }
+                    };
+                }
+            }
+        }
+        // Set the minimum and maximum width of the row window.
+        let spaces = self.spacing * (self.layouts.len() as i32 - 1) + self.padding * 2;
+        let ais = self.this.absrect().size.width - self.this.rect().size.width;
+        if min_width > 0 {
+            self.this.set_min_width(min_width + spaces + ais);
+        } else {
+            self.this.lift_min_width();
+        }
+        if need_max {
+            self.this.set_max_width(max_width + spaces + ais);
+        } else {
+            self.this.lift_max_width();
+        }
+        // Calculate the position and size of each layout
+        let mut test = self.layouts.clone();
+        test.iter_mut().for_each(|x| {
+            let (_, mode) = x;
+            if let LayoutMode::Range { ratio, .. } = mode {
+                *mode = LayoutMode::Ratio(*ratio);
+            }
+        });
+        let result = loop {
+            // Calculate all of the ratios.
+            let sum_ratio = test.iter().fold(0.0, |acc, x| {
+                let (_, mode) = x;
+                match mode {
+                    LayoutMode::Ratio(ratio) => acc + *ratio,
+                    _ => acc,
+                }
+            });
+            let sum_fixed = test.iter().fold(0, |acc, x| {
+                let (_, mode) = x;
+                match mode {
+                    LayoutMode::Fixed(width) => acc + width,
+                    _ => acc,
+                }
+            });
+            // Try to calculate the position and size of each layout.
+            let mut result = HashMap::new();
+            let mut x = self.padding;
+            for &(layout, mode) in &test {
+                let width = match mode {
+                    LayoutMode::Fixed(width) => width,
+                    LayoutMode::Ratio(ratio) => ((size.width - spaces - sum_fixed) as f32 * ratio
+                        / sum_ratio)
+                        .max(0.0) as i32,
+                    _ => panic!("Unreachable branch!"),
+                };
+                let rect = rect!(x, self.padding, width, size.height - self.padding * 2);
+                result.insert(layout, rect);
+                x += width + self.spacing;
+            }
+            // Check the result is in their range and update the test.
+            let mut changed = false;
+            for (i, &(layout, mode)) in self.layouts.iter().enumerate() {
+                if let LayoutMode::Range { min, max, .. } = mode {
+                    let width = result.get(&layout).unwrap().size.width;
+                    if let Some(min) = min {
+                        if width < min {
+                            test[i] = (layout, LayoutMode::Fixed(min));
+                            changed = true;
+                        }
+                    }
+                    if let Some(max) = max {
+                        if width > max {
+                            test[i] = (layout, LayoutMode::Fixed(max));
+                            changed = true;
+                        }
+                    }
+                }
+            }
+            if !changed {
+                break result;
+            }
+        };
+        // Set the height limits of the row window.
+        let mut it_min_height: Option<i32> = None;
+        let mut it_max_height: Option<i32> = None;
+        self.this.foreach(move |child| {
+            let rect = result.get(&child.as_window().get_id());
+            if let Some(rect) = rect {
+                child.as_window().set_absrect(*rect);
+                if let Some(height) = child.as_window().min_height {
+                    if let Some(min_height) = it_min_height {
+                        it_min_height = Some(min_height.max(height));
+                    } else {
+                        it_min_height = Some(height);
+                    }
+                }
+                if let Some(height) = child.as_window().max_height {
+                    if let Some(max_height) = it_max_height {
+                        it_max_height = Some(max_height.min(height));
+                    } else {
+                        it_max_height = Some(height);
+                    }
+                }
+            }
+        });
+        if let Some(min_height) = it_min_height {
+            self.this.set_min_height(min_height);
+        } else {
+            self.this.lift_min_height();
+        }
+        if let Some(max_height) = it_max_height {
+            self.this.set_max_height(max_height);
+        } else {
+            self.this.lift_max_height();
+        }
+    }
+}
+
+impl Row {
+    /// Create a new `Row` widget.
+    pub fn new(rect: Rect, parent: Option<&Window>) -> Widget<Self> {
+        Widget::new("Row", rect, parent)
+    }
+
+    /// Add a layout to the row.
+    pub fn add_layout(&mut self, layout: WindowID, mode: LayoutMode) {
+        self.layouts.push((layout, mode));
+        self.update(self.this.rect().size);
+    }
+
+    /// Remove a layout from the row.
+    pub fn remove_layout(&mut self, layout: &WindowID) {
+        self.layouts.retain(|x| x.0 != *layout);
+        self.update(self.this.rect().size);
+    }
+
+    /// Set the padding of the row.
+    pub fn set_padding(&mut self, padding: i32) {
+        self.padding = padding;
+        self.update(self.this.rect().size);
+    }
+
+    /// Set the spacing of the row.
+    pub fn set_spacing(&mut self, spacing: i32) {
+        self.spacing = spacing;
+        self.update(self.this.rect().size);
+    }
+}
